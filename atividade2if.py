6728120f9ae5ccab497a1da9f2494348efb00b7c
nicholasmaieronvenson <nicholas.maieron.venson14@gmail.com>
2026-05-20 09:56:21 -0300

    Create atividade2if.py
    if mais e menos!!!!

---
+++ atividade2if.py
@@ -0,0 +1,8 @@
+trollvida = 10
+flechasdolink = 20
+if flechasdolink == trollvida  :
+  print ("link win")
+if trollvida > flechasdolink :
+  print ("link dead")
+if flechasdolink > trollvida:
+  print ("link win!")
