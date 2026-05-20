d2429beafe735024c6bbb3176b9b1c187830a8a5
nicholasmaieronvenson <nicholas.maieron.venson14@gmail.com>
2026-05-20 09:20:14 -0300

    Create atividade2revisão.py

---
+++ atividade2revisão.py
@@ -0,0 +1,12 @@
+
+minha_idade = 11
+String = "minha idade "
+#print (String + str( minha_idade)   )
+minha_altura = 1.46
+minha_idade += minha_altura
+#print ("e minha altura " + str(minha_altura)  + " somada a minha idade resulta em " + str(minha_idade) )
+minha_altura2 = True
+minha_idade2 = False
+print ("e minha altura esta " + str(minha_altura2)  + "  e minha idade esta " + str(minha_idade2) )
+# eu tenho 11 anos realmente
+ 
