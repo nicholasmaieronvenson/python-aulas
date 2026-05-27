f3264181e8925310e163d1d2bbcfee2e1b1e3d2e
nicholasmaieronvenson <nicholas.maieron.venson14@gmail.com>
2026-05-27 10:21:49 -0300

    Create atividade3if.py

---
+++ atividade3if.py
@@ -0,0 +1,9 @@
+chovendo = False
+nublado = True
+if chovendo == True:
+ print ("levar guarda-chuva")
+else:
+  if nublado == True :
+    print ("levar guara-chuva")
+  else:
+    print(" nao precisa levar o guarda-chuva")
