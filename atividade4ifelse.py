305d655fcc8f08f396f45a737ade1dcd5e35b617
nicholasmaieronvenson <nicholas.maieron.venson14@gmail.com>
2026-05-27 10:22:43 -0300

    Create atividade4ifelse.py

---
+++ atividade4ifelse.py
@@ -0,0 +1,25 @@
+kakyoin = "vivo"
+DIO = "vivo"
+hierofante_verde = True
+brilho_de_esmeralda = True
+the_world = True
+kakyoin_ir_com_o_jotaro = True
+
+if kakyoin_ir_com_o_jotaro == False:
+  print ("voltou pro hospital com o polnareff")
+else :
+  print ("kakyoin foi pra luta contra o DIO")
+  if brilho_de_esmeralda == True:
+    if the_world == True:
+      kakyoin = "morto"
+    else :
+      DIO = "morto"
+  else:
+    if hierofante_verde == true :
+      DIO = "morto"
+    else :
+      kakyoin = "morto"
+if kakyoin == "morto":
+  print(" kakyoin morreu")
+else :
+  print (" salvaram a holy")
