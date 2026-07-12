92e260fa2505bedfe2710717af383b4f5c92265d
nicholasmaieronvenson <nicholas.maieron.venson14@gmail.com>
2026-07-12 10:59:32 -0300

    Create desafioSobreviveciaCibernetica.py
    67 resenha chocolax

---
+++ desafioSobreviveciaCibernetica.py
@@ -0,0 +1,6 @@
+enregiaDaIA = 100
+nomeDOprota = input("diga seu nome pequeno coagúlo")
+energiadoProta = 100
+jogando = True
+while energiadoProta > 0 and  enregiaDaIA > 0 and jogando == True:
+    
