a87dab9d607928ae6da3490d084a1aef9ee5275d
nicholasmaieronvenson <nicholas.maieron.venson14@gmail.com>
2026-08-02 11:36:01 -0300

    Create aulaFOR1.py
    aula toplistaDeAlunos = ["ALBERTO" , "BOLINHO" , "CURURU" , "CRISTIANO RONADO R7" , "DIO"]
    print (f" você deve escolher entre os 5 alunos para saber o numero deles na chamda {listaDeAlunos}")
    numeroDoAluno =     int(input("fale o numero do aluno do aluno: "))
    if numeroDoAluno <0:
        print ("pode numero negativo não man ☝️​")
    elif numeroDoAluno >4:
        print ("pode numero grande  nao man ☝️ ")
    else:
        print (f"{listaDeAlunos[numeroDoAluno]}")

---
+++ aulaFOR1.py
@@ -0,0 +1,17 @@
+listaDeAlunos = ["ALBERTO" , "BOLINHO" , "CURURU" , "CRISTIANO RONADO R7" , "DIO"]
+print (f" você deve escolher entre os 5 alunos para saber o numero deles na chamda {listaDeAlunos}")
+numeroDoAluno =     int(input("fale o numero do aluno do aluno: "))
+if numeroDoAluno <0:
+    print ("pode numero negativo não man ☝️​")
+elif numeroDoAluno >4:
+    print ("pode numero grande  nao man ☝️ ")
+else:
+    print (f"{listaDeAlunos[numeroDoAluno]}")
+
+
+
+
+
+
+
+
