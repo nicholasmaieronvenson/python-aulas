b27b7e426a3e95c9b56165452d57dedb1624cee9
nicholasmaieronvenson <nicholas.maieron.venson14@gmail.com>
2026-08-26 09:29:55 -0300

    Create aulaFOR3.py

---
+++ aulaFOR3.py
@@ -0,0 +1,11 @@
+jogos = ["free fire" , "roblox" , "hollow knight" , "ubercat overdrive"]
+for jogo in jogos: 
+    if "free fire" == jogo:
+        print ("odeio free fire")
+    elif "roblox" == jogo:
+        print ("roblox é mais ou menos")
+    elif "hollow knight" == jogo:
+        print ("hollow knight é bom")
+    else:
+        print ("ubercat overdrive é o melhor jogo do mundo supera até gta 6")
+    
