cedfe0fb7106eebea7961f0f64022f2122c416b6
nicholasmaieronvenson <nicholas.maieron.venson14@gmail.com>
2026-09-02 09:46:45 -0300

    Create aulaEssaLetraÈdoMEunome.py
    essa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nomeessa letra é do meu nome

---
+++ aulaEssaLetraÈdoMEunome.py
@@ -0,0 +1,21 @@
+nome = input("fala uma palavra pra tentar descobrir meu nome:  ")
+for letra in nome:
+    if "n" == letra :
+        print (f" essa letra é do meu nome {letra}")
+    elif "i" == letra :
+        print (f" essa letra é do meu nome {letra}") 
+    elif "c" == letra :
+        print (f" essa letra é do meu nome {letra}") 
+    elif "h" == letra :
+        print (f" essa letra é do meu nome {letra}") 
+    elif "o" == letra :
+        print (f" essa letra é do meu nome {letra}")
+    elif "l" == letra :
+        print (f" essa letra é do meu nome {letra}") 
+    elif "a" == letra :
+        print (f" essa letra é do meu nome {letra}")
+    elif "s" == letra :
+        print (f" essa letra é do meu nome {letra}")
+    else:
+        print (f"essa letra não é do meu nome {letra}")
+      
